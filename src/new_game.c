20ffc38750bd56df027c3d0c2c6d60b2e0db0682
Tejas M R <tejasmr@outlook.com>
2026-09-14 22:07:52 +0530

    Corrected flag for running shoes (#29)

--- src/new_game.c
+++ src/new_game.c
@@ -45,7 +45,6 @@
 #include "mystery_gift.h"
 #include "union_room_chat.h"
 #include "constants/items.h"
-#include "constants/flags.h"
 
 extern const u8 EventScript_ResetAllMapFlags[];
 
@@ -212,6 +211,7 @@ void NewGameInitData(void)
 
     AddBagItem(ITEM_AUTO_HEALER, 1);
     FlagSet(FLAG_RECEIVED_RUNNING_SHOES);
+    FlagSet(FLAG_SYS_B_DASH);
 }
 
 static void ResetMiniGamesRecords(void)
