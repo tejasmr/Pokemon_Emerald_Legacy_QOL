21d6308d7662cf483ff276dfe2d5d7f2894fcabf
Tejas M R <tejasmr@outlook.com>
2026-09-14 20:41:42 +0530

    Auto Healer: Added Callback (#25)
    * Auto Healer: Added Callback
    
    * Corrected function definition
    
    * Corrected var usage

--- src/item_use.c
+++ src/item_use.c
@@ -56,6 +56,7 @@ static void PlayerFaceHiddenItem(u8);
 static void CheckForHiddenItemsInMapConnection(u8);
 static void Task_OpenRegisteredPokeblockCase(u8);
 static void ItemUseOnFieldCB_Bike(u8);
+static void ItemUseOnFieldCB_AutoHealer(u8);
 static void ItemUseOnFieldCB_Rod(u8);
 static void ItemUseOnFieldCB_Itemfinder(u8);
 static void ItemUseOnFieldCB_Berry(u8);
@@ -220,8 +221,21 @@ void ItemUseOutOfBattle_Bike(u8 taskId)
 void ItemUseOutOfBattle_AutoHealer(u8 taskId)
 {
     HealPlayerParty();
+    sItemUseOnFieldCB = ItemUseOnFieldCB_AutoHealer;
+    SetUpItemUseOnFieldCallback(taskId);
+}
+
+static void ItemUseOnFieldCB_AutoHealer(u8 taskId)
+{
     StringExpandPlaceholders(gStringVar4, gText_PartyWasRestored);
-    DisplayItemMessage(taskId, FONT_NORMAL, gStringVar4, DoNothingAfterItemCompletion);
+    if (gTasks[taskId].tUsingRegisteredKeyItem != TRUE)
+    {
+        DisplayItemMessage(taskId, FONT_NORMAL, gStringVar4, CloseItemMessage);
+    }
+    else
+    {
+        DisplayItemMessageOnField(taskId, gStringVar4, Task_CloseCantUseKeyItemMessage);
+    }
 }
 
 static void ItemUseOnFieldCB_Bike(u8 taskId)
