9c07f312b45ba5fa1b95a7fe350f4f7928cd419c
tejasmr <tejasmr@outlook.com>
2026-09-14 18:24:41 +0530

    Fixed import

--- src/new_game.c
+++ src/new_game.c
@@ -45,7 +45,7 @@
 #include "mystery_gift.h"
 #include "union_room_chat.h"
 #include "constants/items.h"
-#include "flags.h"
+#include "constants/flags.h"
 
 extern const u8 EventScript_ResetAllMapFlags[];
 
