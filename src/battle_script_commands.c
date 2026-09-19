8138d797651183081ad5927e6a9b60b3e7985360
tejasmr <tejasmr@outlook.com>
2026-09-19 12:48:23 +0530

    Always catch pokemon

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -10008,7 +10008,7 @@ static void Cmd_handleballthrow(void)
 
             for (shakes = 0; shakes < BALL_3_SHAKES_SUCCESS && Random() < odds; shakes++);
 
-            if (gLastUsedItem == ITEM_MASTER_BALL)
+            if (true)
                 shakes = BALL_3_SHAKES_SUCCESS; // why calculate the shakes before that check?
 
             BtlController_EmitBallThrowAnim(BUFFER_A, shakes);
