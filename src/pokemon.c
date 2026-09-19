da901b47f02bcf67a9ab373e868e7ccf0c4012b6
Tejas M R <tejasmr@outlook.com>
2026-09-19 14:45:06 +0530

    Fix preferred nature selection for low-speed species (#39)

--- src/pokemon.c
+++ src/pokemon.c
@@ -2125,7 +2125,7 @@ u8 GetSpeciesPreferredNature(u16 species)
     u8 spDefense = gSpeciesInfo[finalSpecies].baseSpDefense;
 
     if (gSpeciesInfo[finalSpecies].baseSpeed < 70)
-        return attack > spAttack ? NATURE_JOLLY : NATURE_TIMID;
+        return attack > spAttack ? NATURE_BRAVE : NATURE_QUIET;
 
     if ((IS_TYPE_PHYSICAL(type1) && IS_TYPE_SPECIAL(type2))
      || (IS_TYPE_SPECIAL(type1) && IS_TYPE_PHYSICAL(type2)))
