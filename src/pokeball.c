dddb7b236079123ff41da04d183cf1515d3c33bf
tejasmr <tejasmr@outlook.com>
2026-09-20 19:03:36 +0530

    Adjust player Pokémon send-out animation timing for improved visual flow

--- src/pokeball.c
+++ src/pokeball.c
@@ -910,7 +910,7 @@ static void SpriteCB_BallThrow_CaptureMon(struct Sprite *sprite)
 
 static void SpriteCB_PlayerMonSendOut_1(struct Sprite *sprite)
 {
-    sprite->data[0] = 25;
+    sprite->data[0] = 12;
     sprite->data[2] = GetBattlerSpriteCoord(sprite->sBattler, BATTLER_COORD_X_2);
     sprite->data[4] = GetBattlerSpriteCoord(sprite->sBattler, BATTLER_COORD_Y_PIC_OFFSET) + 24;
     sprite->data[5] = -30;
