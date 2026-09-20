194ac4cd422f4eb9dd0bd2d843c1b17ee7fa8b88
Tejas M R <tejasmr@outlook.com>
2026-09-20 10:27:31 +0530

    Starter is now shiny (#45)

--- src/script_pokemon_util.c
+++ src/script_pokemon_util.c
@@ -77,6 +77,7 @@ u8 ScriptGiveMon(u16 species, u8 level, u16 item, u32 unused1, u32 unused2, u8 u
     u8 heldItem[2];
     struct Pokemon mon;
 
+    FlagSet(FLAG_SHINY_CREATION);
     CreateMon(&mon, species, level, USE_RANDOM_IVS, FALSE, 0, OT_ID_PLAYER_ID, 0);
     heldItem[0] = item;
     heldItem[1] = item >> 8;
